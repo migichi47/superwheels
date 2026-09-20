17509153f0a38d31793a75cc7750b40a69fc42e8
Peter Migichi <petermigichi47@gmail.com>
2026-09-20 09:17:08 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import { Layout } from "./Layout";
 import { LandingPage } from "./pages/LandingPage";
 import { ProductsPage } from "./pages/ProductsPage";
 import { AboutUsPage } from "./pages/AboutUsPage";
+import api from "./axios";
 
 export default function App() {
+  console.log("BASE URL:", api.defaults.baseURL);
+
   return (
     <Routes>
       <Route path="" element={<Layout />}>
